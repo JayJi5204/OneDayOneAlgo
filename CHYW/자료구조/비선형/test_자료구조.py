5c293932a9533c9bd8054f5c4eff66cfe3d8a088
chldyddnjs <dyddnjsdl321@naver.com>
2025-12-17 04:54:21 +0000

    251217_여행경로

---
+++ CHYW/자료구조/비선형/test_자료구조.py
@@ -0,0 +1,19 @@
+#pytest
+def test_minimum_spanning_tree():
+    from 최소신장트리 import kruskal, prim
+    # 크루스칼 알고리즘 테스트
+    n = 4
+    edges = [(0, 1, 1), (0, 2, 3), (1, 2, 1), (1, 3, 4), (2, 3, 2)]
+    mst_cost, mst_edges = kruskal(n, edges)
+    assert mst_cost == 4
+    assert set(mst_edges) == {(0, 1, 1), (1, 2, 1), (2, 3, 2)} 
+    # 프림 알고리즘 테스트
+    graph = [
+        [(1, 1), (2, 3)],
+        [(0, 1), (2, 1), (3, 4)],
+        [(0, 3), (1, 1), (3, 2)],
+        [(1, 4), (2, 2)]
+    ]
+    mst_cost_prim, mst_edges_prim = prim(0, graph)
+    assert mst_cost_prim == 4
+    assert set(mst_edges_prim) == {(0, 1, 1), (1, 2, 1), (2, 3, 2)}
