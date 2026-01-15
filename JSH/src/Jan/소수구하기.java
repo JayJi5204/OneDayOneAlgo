4fa5c77a5cb6ac8a29548e516b14958238769536
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-01-15 18:04:02 +0900

    260115_소수 구하기 (#119)

---
+++ JSH/src/Jan/소수구하기.java
@@ -0,0 +1,40 @@
+package Jan;
+
+import java.util.Scanner;
+
+public class 소수구하기 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+
+		boolean[] check = new boolean[N + 1];
+
+		for (int i = 2; i < check.length; i++) {
+			check[i] = true;
+
+		}
+
+		for (int i = 2; i <= N; i++) {
+			if (check[i] == false) {
+				continue;
+			}
+
+			for (int j = i * i; j <= N; j += i) {
+				check[j] = false;
+
+			}
+
+		}
+
+		for (int i = M; i <= N; i++) {
+			if(check[i]) {
+				System.out.println(i);
+			}
+
+		}
+
+	}
+}
