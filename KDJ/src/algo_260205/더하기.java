225acd634c59f37f00c024bc0623a266816b1ca5
KIM DJ <132763253+daejung-kim96@users.noreply.github.com>
2026-02-05 23:00:54 +0900

    260205_1,2,3 더하기 (#172)

---
+++ KDJ/src/algo_260205/더하기.java
@@ -0,0 +1,31 @@
+package algo_260205;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 더하기 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int  T = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[12];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		for(int i = 4; i <= 11; i++) {
+			dp[i] =+ dp[i-3]+ dp[i-2]+ dp[i-1];
+		}
+ 		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int tc = 0; tc < T; tc++) {
+			int input = Integer.parseInt(br.readLine());
+			sb.append(dp[input]).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
